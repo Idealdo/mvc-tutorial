41d908f08da11f334649dd81a49d0cd9e0fd5a6d
Idealdo <idealdofelix@gmail.com>
2023-02-23 10:48:42 -0600

    Adding MVC sample files

---
+++ src/test/java/com/example/mvcsample/MvcivanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcivanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
